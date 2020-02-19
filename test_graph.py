fb0ee79b1389d650f1eed62a5c9755d9b5ff7fe9
maxime <maxime.moutet@etu.univ-amu.fr>
2020-02-19 19:10:56 +0100

    fez

--- test_graph.py
+++ test_graph.py
@@ -1,24 +1,28 @@
-__Author__ = "aillet"
-__Filename__ = "test_graph"
-__Creationdate__ = "19/02/2020"
-import graph.py
+from graph import*
 
 
-graph = DirectedGraph()
+graph = DirectedGraph.empty_graph()
+
 graph.add_vertex(1)
-graph.add_edge(1,2,1)
-graph.add_edge(2,1,1)
-graph.add_edge(2,3,1)
+graph.add_edge(1, 2, 1)
+graph.add_edge(2, 1, 1)
+graph.add_edge(2, 3, 1)
 
 print(graph.vertices)
 print(len(graph))
 print(graph[2])
 print(graph)
 
-
 for vertex in graph:
     print(vertex)
 
-
-graph.remove_edge(1,2)
+graph.remove_edge(1, 2)
 print(graph)
+graph.change_weight(2, 3, 10)
+print(graph)
+
+
+G = graph.to_networkx()
+plt.figure()
+nx.draw(G, with_labels=True)
+plt.show()
