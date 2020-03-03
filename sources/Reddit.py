5c9c0036cd7f198a402570f053b0a5dd85733a47
maxime <maxime.moutet@etu.univ-amu.fr>
2020-03-03 23:40:27 +0100

    hihi

--- sources/Reddit.py
+++ sources/Reddit.py
@@ -1,13 +1,17 @@
-redditGraph = create_graph("/home/aillet/Bureau/Infromatique/Projet_Structures_Algorithmes/soc-redditHyperlinks-title.tsv")
+redditGraph = create_graph(
+    "/home/aillet/Bureau/Infromatique/Projet_Structures_Algorithmes/soc-redditHyperlinks-title.tsv")
 
-def  top10(subreddit):
+
+def top10(subreddit):
     nbr = []
     for i in subreddit:
-        nbr += [(i,len(redditGraph[i]))]
-    nbr.sort(key = lambda x: x[1],reverse=True)
-    return(nbr[:10])
+        nbr += [(i, len(redditGraph[i]))]
+    nbr.sort(key=lambda x: x[1], reverse=True)
+    return nbr[:10]
+
 
 print(top10(redditGraph))
+
 
 def isolated(subreddit):
     nbr = []
@@ -16,13 +20,14 @@ def isolated(subreddit):
         nbr += [(i, len(redditGraph[i]))]
     nbr.sort(key=lambda x: x[1])
 
-
     while nbr[isolated][1] == 0:
         isolated += 1
 
     return isolated
 
+
 print(isolated((redditGraph)))
+
 
 def activity_Part_Two_Percent(subreddit):
     max = len(subreddit)
@@ -33,22 +38,20 @@ def activity_Part_Two_Percent(subreddit):
     nbr = []
 
     for i in subreddit:
-        nbr += [(i,len(redditGraph[i]))]
-    nbr.sort(key = lambda x: x[1],reverse=True)
+        nbr += [(i, len(redditGraph[i]))]
+    nbr.sort(key=lambda x: x[1], reverse=True)
 
     for i in range(max):
         nbr_links += nbr[i][1]
 
-
     for i in range(two_percent):
         nbr_links_two_percent += nbr[i][1]
 
+    activityPartTwoPercent = (100 * nbr_links_two_percent) / nbr_links
+    return activityPartTwoPercent
 
-
-    activityPartTwoPercent = (100 * nbr_links_two_percent)/nbr_links
-    return (activityPartTwoPercent)
 
 print(activity_Part_Two_Percent(redditGraph))
-un,deux = redditGraph.Dijkstra_binary_heap('disney')
+un, deux = redditGraph.Dijkstra_binary_heap('disney')
 print(1)
-print(redditGraph.DijkstraPath(deux,'vegan'))
+print(redditGraph.DijkstraPath(deux, 'vegan'))
