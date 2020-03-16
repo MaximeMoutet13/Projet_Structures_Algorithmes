a18a2721ebee6b250987cdbfac76d59544b211c7
aillet <aillet.gabriel05@gmail.com>
2020-03-16 14:13:37 +0100

    v

--- sources/Reddit.py
+++ sources/Reddit.py
@@ -53,11 +53,15 @@ def activity_Part_Two_Percent(subreddit):
 
     activityPartTwoPercent = (100 * nbr_links_two_percent) / nbr_links
     return activityPartTwoPercent
+
+un, deux = redditGraph.Dijkstra_from_u_to_v('disney','vegan')
+trois,quate = redditGraph.Dijkstra_from_u_to_v('greenbaypackers','missouripolitics')
+
+
+
+
 print('Les 10 subreddit ayant le plus de liens avec d\'autre subbreddit :',top10(redditGraph))
 print('Il y a ',isolated((redditGraph)), 'subreddit qui sont isolés.')
 print('La part des 2% de subreddit les plus actif est de',activity_Part_Two_Percent(redditGraph))
-un, deux = redditGraph.Dijkstra_from_u_to_v('disney','vegan')
-
-print(redditGraph.DijkstraPath(deux,'vegan'))
-un,deux = redditGraph.Dijkstra_from_u_to_v('greenbaypackers','missouripolitics')
-print(redditGraph.DijkstraPath(deux,'missouripolitics'))
+print('Le chemin partant de Disney et allant à vegan est :',redditGraph.DijkstraPath(deux,'vegan'))
+print('Le chemin partant de greenbaypackers et allant à missouripolitics est :',redditGraph.DijkstraPath(deux,'missouripolitics'))
